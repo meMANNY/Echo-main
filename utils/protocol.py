a55b39cbb05c9c883dca397deed27623e2325369
Aman Tripathi <amanttripathi02@gmail.com>
2026-05-28 14:58:13 +0530

    Implemented a Gate keeper function to read and decode incoming messages, ensuring proper error handling and connection management. Updated exception handling to use consistent exception codes for disconnection scenarios.

--- utils/protocol.py
+++ utils/protocol.py
@@ -3,7 +3,7 @@ import socket
 import msgpack
 
 from utils.constants import FMT, HEADER_MSG_LEN, HEADER_TYPE_LEN
-from utils.exceptions import ExceptionCode, RequestException
+from utils.exceptions import  ExceptionCodes, RequestException
 from utils.socket_functions import recvall
 from utils.types import HeaderCode, SocketMessage
 
@@ -11,7 +11,7 @@ from utils.types import HeaderCode, SocketMessage
 def build_header(type_code: HeaderCode, body_len: int) -> bytes:
 
     """ Build a 16 byte header 
-    
+
     type_code == HeaderCode.*
     body_len == length of the body in bytes"""
 
@@ -29,5 +29,108 @@ def send_message(sock, type_code : HeaderCode, body: bytes = b"") -> None:
     header = build_header(type_code, len(body))
     sock.sendall(header+body)
 
+def receive_message(sock : socket.socket) -> SocketMessage:
+
+    """ Function to Read a 16-byte header , 
+    decode it and returns the message type and body
+    Acts as a GateKeeper for incoming network traffics"""
+
+    # Read the type byte
+
+    try:
+        type_bytes = sock.recv(HEADER_TYPE_LEN)
+    
+    except OSError as e:
+        logging.error(msg = f"Error receiving message type: {e}")
+        raise RequestException(
+            msg = f"Connection error:{e}",
+            code = ExceptionCodes.DISCONNECT)
+
+    if not type_bytes:
+        raise RequestException(
+            msg = "Connection closed by peer",
+            code = ExceptionCodes.DISCONNECT)
+    
+    type_char = type_bytes.decode(FMT)  
 
 
+    #validate against the type header codes
+
+    try:
+        type_code = HeaderCode(type_char)
+    
+    except ValueError:
+        logging.error(msg = f"Invalid message type in header: {type_char}")
+        raise RequestException(
+            msg = "Invalid message type",
+            code = ExceptionCodes.INVALID_HEADER)
+    
+    # Read the length field
+
+    try:
+
+        len_bytes = recvall(sock, HEADER_MSG_LEN)
+    
+    except OSError as e:
+        logging.error(msg = f"Error receiving message length: {e}")
+        raise RequestException(
+            msg = f"Connection error:{e}",
+            code = ExceptionCodes.DISCONNECT)
+    
+    if len(len_bytes) < HEADER_MSG_LEN:
+        logging.error(msg = f"Incomplete message length received: {len_bytes}")
+        raise RequestException(
+            msg = "Incomplete message length received",
+            code = ExceptionCodes.INVALID_HEADER)
+
+    try:
+        body_len = int(len_bytes.decode(FMT).strip())
+    except ValueError:
+
+        raise RequestException(
+            msg = f"Invalid message length in header: {len_bytes}",
+            code = ExceptionCodes.INVALID_HEADER)
+    
+    # Read the message body completely using recvall
+
+    try:
+        body = recvall(sock, body_len)
+
+    except OSError as e:
+        logging.error(msg = f"Error receiving message body: {e}")
+        raise RequestException(
+            msg = f"Connection error:{e}",
+            code = ExceptionCodes.DISCONNECT)
+
+    if len(body) < body_len:
+        raise RequestException(
+            msg = "Incomplete message body received",
+            code = ExceptionCodes.INCOMPLETE)
+    
+    # if the type is error, decode and raise the error message
+
+    if type_code == HeaderCode.ERROR:
+
+        try:
+            err_dict = msgpack.unpackb(body)
+            err = RequestException.from_dict(err_dict)
+
+        except Exception as e:
+            logging.error(msg = f"Error decoding error message body: {e}")
+            raise RequestException(
+                msg = "Failed to decode error message body",
+                code = ExceptionCodes.BAD_REQUEST)
+        
+        raise err
+
+    return SocketMessage({
+        "type": type_code,
+        "query": body
+    })
+
+
+    
+
+    
+
+    
