8ef5a88dc689097ee8becf89e4fad7b61e62f8ac
Aman Tripathi <amanttripathi02@gmail.com>
2026-08-29 04:50:56 +0530

    created layout and label widgets for start window

--- client/ui/start_window.py
+++ client/ui/start_window.py
@@ -7,3 +7,25 @@ from PyQt5.QtWidgets import (
 from client.ui.basic_config_window import BasicConfigWindow
 
 logger = logging.getLogger(__name__)
+
+class StartWindow(QWidget):
+    def __init__(self, core):
+        super().__init__()
+        self.core = core
+        self.init_ui()  
+
+    def init_ui(self):
+        self.setWindowTitle("Echo- Welcome")
+        self.setFixedSize(380, 200)  # Set a fixed size for the window
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(30,25,30,25)  # Set margins for the layout
+        layout.setSpacing(12)
+
+        title_label = QLabel("Welcome to Echo!")
+        title_label.setStyleSheet("font-size: 18px; font-weight: bold;")
+        title_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(title_label)
+
+        
+
