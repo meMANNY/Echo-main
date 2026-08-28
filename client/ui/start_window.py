32bda9a240477b3f77bf369647d41dc765a205bd
Aman Tripathi <amanttripathi02@gmail.com>
2026-08-29 04:45:22 +0530

    created a clean , focused window where user inputs his usernmae

---
+++ client/ui/start_window.py
@@ -0,0 +1,9 @@
+import logging
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QHBoxLayout,QMessageBox
+)
+
+from client.ui.basic_config_window import BasicConfigWindow
+
+logger = logging.getLogger(__name__)
